b606b32c9e455a5eaacf403e9d9eac8f97254aee
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-06-16 18:10:59 +0100

    Add CRUD Route and template pages for CRUD actions

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.secret_key = '7764d031a1424bf8b12357f7ebb05681'
 app.config["MONGO_DBNAME"] = os.getenv("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
@@ -27,8 +28,7 @@ def builds():
         "title": "Builds",
         "files": "builds"
     }
-    ##return render_template("builds.hmtl", content)
-    pass
+    return render_template("builds.html", **content)
 
 @app.route("/contact_us")
 def contact():
@@ -37,32 +37,72 @@ def contact():
         "title": "Contact Us",
         "files": "contact"
     }
-    ##return render_template("contact.hmtl", content)
-    pass
+    return render_template("contact.html", **content)
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-
+    
     content = {
         "title": "Register",
         "files": "register"
     }
-    ##return render_template("register.hmtl", content)
-    pass
+    return render_template("register.html", **content)
 
-@app.route("/login")
+@app.route("/login", methods=['POST', 'GET'])
 def login():
+    
+    if request.method == 'POST':
+
+        pass
 
     content = {
         "title": "Login",
         "files": "login"
     }
-    ##return render_template("register.hmtl", content)
+    return render_template("login.html", **content)
+
+@app.route("/logout")
+def logout():
+    
     pass
+
+@app.route("/edit")
+def edit_record():
+
+    content = {
+        "title": "Edit a Build",
+        "files": "edit"
+    }
+    return render_template("edit.html", **content)
 
 ## CRUD Routes
 
+@app.route("/create_record")
+def create_record():
+    
 
+
+    content = {
+        "title": "Create a Build",
+        "files": "create"
+    }
+    return render_template("create.html", **content)
+
+@app.route("/insert_record", methods=["POST"])
+def insert_record():
+
+    pass
+
+@app.route("/update_record")
+def update_record():
+
+    pass
+
+@app.route("/delete_record/<build_id>")
+def delete_record(build_id):
+
+    
+    pass
 
 ##if __name__ == '__main__':
     ##app.run(host=os.environ.get('IP'),
