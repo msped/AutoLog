ae1618a141d5c16adb9b629a5aa5936ab9f52680
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-07-30 22:11:50 +0100

    Fixed formatting of js

--- static/js/create.js
+++ static/js/create.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         });
          $('#build-total').html(buildPrice);
          $('input[name="total"]').val(buildPrice);
-    })
+    });
 
     $(window).keydown(function(event){
         if(event.keyCode == 13) {
@@ -18,90 +18,88 @@ $(document).ready(function () {
     $('#add-bodykit').on('click', function(){
         part_id = $('#bodykit-categories').find(":selected").val();
         title = $('#bodykit-categories').find(":selected").text();
-        var template = '<tr>\
-                        <td scope="row">'
-                            + title +
-                        '</td>\
-                        <td>\
-                            <input type="text" class="form-control input-sm" name="bodykit_'+ part_id +'_product" required>\
-                        </td>\
-                        <td>\
-                            <input type="url" class="form-control input-sm" name="bodykit_'+ part_id +'_link" required>\
-                        </td>\
-                        <td>\
-                            <input type="number" class="form-control input-sm" name="bodykit_'+ part_id +'_price" step="any" id="part-price" required>\
-                        </td>\
-                        <td>\
-                            <i class="far fa-times-circle" id="delete-row"></i>\
-                        </td>\
-                    </tr>'
+        var template = '<tr>' +
+                        '<td scope="row">' + title + '</td>' +
+                        '<td>' +
+                            '<input type="text" class="form-control input-sm" name="bodykit_'+ part_id +'_product" required>' +
+                        '</td>' +
+                        '<td>' +
+                            '<input type="url" class="form-control input-sm" name="bodykit_'+ part_id +'_link" required>' +
+                        '</td>' +
+                        '<td>' +
+                            '<input type="number" class="form-control input-sm" name="bodykit_'+ part_id +'_price" step="any" id="part-price" required>' +
+                        '</td>' +
+                        '<td>' +
+                            '<i class="far fa-times-circle" id="delete-row"></i>' +
+                        '</td>' +
+                    '</tr>';
         $('#bodykit-table').append(template);
-    })
+    });
 
     $('#add-engine').on('click', function(){
         part_id = $('#engine-categories').find(":selected").val();
         title = $('#engine-categories').find(":selected").text();
-        var template = '<tr>\
-                            <td scope="row">'+ title +'</td>\
-                        <td>\
-                            <input type="text" class="form-control input-sm" name="engine_'+ part_id +'_product" required>\
-                        </td>\
-                        <td>\
-                            <input type="url" class="form-control input-sm" name="engine_'+ part_id +'_link" required>\
-                        </td>\
-                        <td>\
-                            <input type="number" class="form-control input-sm" name="engine_'+ part_id +'_price" step="any" id="part-price" required>\
-                        </td>\
-                        <td>\
-                            <i class="far fa-times-circle" id="delete-row"></i>\
-                        </td>\
-                    </tr>'
+        var template = '<tr>' +
+                            '<td scope="row">' + title + '</td>' +
+                            '<td>' +
+                                '<input type="text" class="form-control input-sm" name="engine_'+ part_id +'_product" required>' +
+                            '</td>' +
+                            '<td>' +
+                                '<input type="url" class="form-control input-sm" name="engine_'+ part_id +'_link" required>' +
+                            '</td>' +
+                            '<td>' +
+                                '<input type="number" class="form-control input-sm" name="engine_'+ part_id +'_price" step="any" id="part-price" required>' +
+                            '</td>' +
+                            '<td>' +
+                                '<i class="far fa-times-circle" id="delete-row"></i>' +
+                            '</td>' +
+                        '</tr>';
         $('#engine-table').append(template);
-    })
+    });
 
     $('#add-running-gear').on('click', function(){
         part_id = $('#running-gear-categories').find(":selected").val();
         title = $('#running-gear-categories').find(":selected").text();
-        var template = '<tr>\
-                            <td scope="row">'+ title +'</td>\
-                        <td>\
-                            <input type="text" class="form-control input-sm" name="running_'+ part_id +'_product" required>\
-                        </td>\
-                        <td>\
-                            <input type="url" class="form-control input-sm" name="running_'+ part_id +'_link" required>\
-                        </td>\
-                        <td>\
-                            <input type="number" class="form-control input-sm" name="running_'+ part_id +'_price" step="any" id="part-price" required>\
-                        </td>\
-                        <td>\
-                            <i class="far fa-times-circle" id="delete-row"></i>\
-                        </td>\
-                    </tr>'
+        var template = '<tr>' +
+                            '<td scope="row">' + title + '</td>' +
+                            '<td>' +
+                                '<input type="text" class="form-control input-sm" name="running_'+ part_id +'_product" required>' +
+                            '</td>' +
+                            '<td>' +
+                                '<input type="url" class="form-control input-sm" name="running_'+ part_id +'_link" required>' +
+                            '</td>' +
+                            '<td>' +
+                                '<input type="number" class="form-control input-sm" name="running_'+ part_id +'_price" step="any" id="part-price" required>' +
+                            '</td>' +
+                            '<td>' +
+                                '<i class="far fa-times-circle" id="delete-row"></i>' +
+                            '</td>' +
+                        '</tr>';
         $('#running-gear-table').append(template);
-    })
+    });
 
     $('#add-interior').on('click', function(){
         part_id = $('#interior-categories').find(":selected").val();
         title = $('#interior-categories').find(":selected").text();
-        var template = '<tr>\
-                            <td scope="row">'+ title +'</td>\
-                        <td>\
-                            <input type="text" class="form-control input-sm" name="interior_'+ part_id +'_product" required>\
-                        </td>\
-                        <td>\
-                            <input type="url" class="form-control input-sm" name="interior_'+ part_id +'_link" required>\
-                        </td>\
-                        <td>\
-                            <input type="number" class="form-control input-sm" name="interior_'+ part_id +'_price" step="any" id="part-price" required>\
-                        </td>\
-                        <td>\
-                            <i class="far fa-times-circle" id="delete-row"></i>\
-                        </td>\
-                    </tr>'
+        var template = '<tr>' +
+                            '<td scope="row">' + title + '</td>' +
+                            '<td>' +
+                                '<input type="text" class="form-control input-sm" name="interior_'+ part_id +'_product" required>' +
+                            '</td>' +
+                            '<td>' +
+                                '<input type="url" class="form-control input-sm" name="interior_'+ part_id +'_link" required>' +
+                            '</td>' +
+                            '<td>' +
+                                '<input type="number" class="form-control input-sm" name="interior_'+ part_id +'_price" step="any" id="part-price" required>' +
+                            '</td>' +
+                            '<td>' +
+                                '<i class="far fa-times-circle" id="delete-row"></i>' +
+                            '</td>' +
+                        '</tr>';
         $('#interior-table').append(template);
-    })
+    });
 
     $('tbody').on('click', '#delete-row', function(){
         $(this).closest('tr').remove();
-    })
-})
+    });
+});
