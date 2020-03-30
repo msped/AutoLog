834fa0e3590b309edbb68a7c70fc49d88c7bc7e5
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-03-30 21:32:15 +0100

    removed url validator as causing page to crash

--- static/js/create.js
+++ static/js/create.js
@@ -114,20 +114,4 @@ $(document).ready(function () {
     $('tbody').on('click', '#delete-row', function(){
         $(this).closest('tr').remove();
     });
-});    
-    
-function validateURL(textval) {
-    var urlregex = new RegExp( "^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
-    return urlregex.test(textval);
-}
-
-$("tbody").on('change paste', 'input[type=url]', function() {
-    var url = $(this).val();
-    console.log(url)
-    if(validateURL(url)) {
-        $(this).css('border-color', 'green');
-    } else {
-        $(this).css('border-color', 'red');
-    }
 });
-
